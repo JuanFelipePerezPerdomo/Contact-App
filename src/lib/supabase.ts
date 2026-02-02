c67610cc66c12064b8e693314b050125c4f1cfd8
Alejandro <aflecodev@gmail.com>
2026-02-02 15:00:36 +0000

    Fixed: refreshtoken

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
-import { Platform } from 'react-native';
-
+import { AppState, Platform } from 'react-native';
 {/* Crear un storage personalizado que solo funcione en el cliente
 Este Custom Storages se hizo por incompatibilidad para la version Web
 es una solucion temporal, creo que lo ideal seria hacer carpetas para controlar versiones 
@@ -45,4 +44,14 @@ export const supabase = createClient(
       detectSessionInUrl: false,
     },
   }
-)
+);
+
+AppState.addEventListener('change', (state) => {
+  if (state === 'active') {
+    // Cuando volvemos a la app, forzamos a Supabase a despertar y chequear el token
+    supabase.auth.startAutoRefresh();
+  } else {
+    // Cuando se va a segundo plano, le decimos que pare para no bugearse
+    supabase.auth.stopAutoRefresh();
+  }
+});
